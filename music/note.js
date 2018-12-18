cf5db2ad18803b99e7b4cdadfe892cce11cc83f2
Tobias Skarhed <tobias.skarhed@gmail.com>
2018-12-18 19:11:27 +0100

    Test setup

---
+++ music/note.js
@@ -0,0 +1,8 @@
+export default class Note {
+    constructor(note, octave = 0, sharp = false){
+        this.note = note;
+        this.octave = octave;
+        this.sharp = sharp;
+    }
+
+};
---
+++ music/note.test.js
@@ -0,0 +1,19 @@
+import Note from './note';
+let note;
+
+describe('Note class', () => {
+    beforeAll(() => {
+        note = new Note('F', 1);
+    });
+
+    test('constructor should set values', () => {
+        expect(note.octave).toBe(1);
+        expect(note.note).toBe('F');
+    });
+    test('set initial octave to 0 and sharp false', () => {
+        note = new Note('F');
+        expect(note.octave).toBe(0);
+        expect(note.sharp).toBe(false);
+    });
+
+});
