e5f2ae43d4a3ca7dff119815cae8c1f0af533e04
Tobias Skarhed <tobias.skarhed@gmail.com>
2019-07-22 12:10:06 +0200

    Add a header to all pages

--- src/layout/menu.jsx
+++ src/layout/menu.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 
-import {NavLink, Switch} from "react-router-dom";
+import { NavLink, Switch } from "react-router-dom";
 
+import Ornament from "../../public/ornament.svg";
 import "./layout.css";
 import "../style.css";
 
 let routes = [
-    {
-        name: "My songs",
-        path: "/",
-        icon: "archive"
-    },{
-        name: "New songs",
-        path: "/new-song",
-        icon: "plus"
-    },
+  {
+    name: "My songs",
+    path: "/",
+    icon: "archive"
+  },
+  {
+    name: "New songs",
+    path: "/new-song",
+    icon: "plus"
+  }
 ];
 
-export default function Menu(){
-    return (
-        <div className="menu menu">
-            <i className="im im-menu menu menuIcon"></i>
-            <div className="menu menuItems">
-                {routes.map((route, i) => {
-                    return <NavLink key={i} className="menu menuItem" to={route.path}><i className={`im im-${route.icon}`}></i>{route.name}</NavLink>
-                })}
-            </div>
-        </div>
-    );
+export default function Menu() {
+  return (
+    <div className="menu menu">
+      <NavLink className="menu menuItem" to="/">
+        My songs
+      </NavLink>
+      <a href="https://www.freepik.com/free-vector/vintage-ornaments-kit_1155572.htm">
+        <img
+          src={Ornament}
+          alt="Ornament Designed by Asmaarzq"
+          className="ornament"
+        />
+      </a>
+
+      <NavLink className="menu menuItem" to="new-song">
+        New songs
+      </NavLink>
+    </div>
+  );
 }
--- src/music/song.jsx
+++ src/music/song.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import AddNote from "./add-note";
@@ -6,125 +6,150 @@ import Note from "./note";
 import NoteOrder from "./noteOrder.js";
 
 import Header from "../layout/header";
-import Ornament from "../../public/ornament.svg";
 
 let localStorage = window.localStorage;
 
-export default class Song extends Component{
-    constructor(props){
-        super(props);
-        let song = JSON.parse(localStorage.getItem("songs")).find((song) => {
-            return song.id == props.match.params.id;
+export default class Song extends Component {
+  constructor(props) {
+    super(props);
+    let song = JSON.parse(localStorage.getItem("songs")).find(song => {
+      return song.id == props.match.params.id;
+    });
+
+    console.log(song);
+    this.state = {
+      selected: undefined,
+      ...song
+    };
+
+    //Check for keypress for navigation
+    document.addEventListener("keydown", e => this.onKeyPress(e));
+  }
+
+  onKeyPress(event) {
+    let key = event.code;
+    console.log(event);
+
+    let orderOfNotes = Object.keys(NoteOrder);
+    let currentIndex = orderOfNotes.indexOf(
+      this.state.notes[this.state.selected]
+    );
+
+    switch (key) {
+      case "Escape":
+        this.setState({ selected: undefined });
+        break;
+      case "ArrowRight":
+        this.setState({
+          selected:
+            this.state.selected == undefined
+              ? 0
+              : (this.state.selected + 1) % (this.state.notes.length + 1)
         });
+        break;
+      case "ArrowLeft":
+        this.setState({
+          selected:
+            this.state.selected == undefined
+              ? this.state.notes.length
+              : this.state.selected == 0
+              ? this.state.notes.length
+              : (this.state.selected - 1) % this.state.notes.length
+        });
+        break;
 
-        console.log(song);
-        this.state = {
-            selected: undefined,
-            ...song
-        }
-
-        //Check for keypress for navigation
-        document.addEventListener("keydown", (e) => this.onKeyPress(e));
-    }
-
-    onKeyPress(event){
-        let key = event.code;
-        console.log(event);
-
-        let orderOfNotes = Object.keys(NoteOrder);
-        let currentIndex = orderOfNotes.indexOf(this.state.notes[this.state.selected]);
-
-        switch(key){
-            case 'Escape':
-                this.setState({selected:undefined});
-                break;
-            case 'ArrowRight':
-                this.setState({selected: (this.state.selected == undefined ? 0 : (this.state.selected+1) % (this.state.notes.length +1 )) });
-                break;
-            case 'ArrowLeft':
-                this.setState({selected: (this.state.selected == undefined ? this.state.notes.length : this.state.selected == 0 ? this.state.notes.length : (this.state.selected-1) % this.state.notes.length) });
-                break;
-
-                //Consider if these should loop inifintely
-            case 'ArrowUp':
-                this.updateNote(this.state.selected, orderOfNotes[ currentIndex == orderOfNotes.length-1 ? orderOfNotes.length-1: currentIndex + 1 ]);
-                break;
-            case 'ArrowDown':
-                this.updateNote(this.state.selected, orderOfNotes[ currentIndex == 0 ? 0 : currentIndex - 1]);
-                break;
-            case 'Backspace':
-            case 'Delete':
-                this.removeNote(this.state.selected);
-                if(this.state.selected == this.state.notes.length){
-                    this.setState({selected: this.state.notes.length-1});
-                }
-                break;
-            case 'Enter':
-                if(this.state.selected == this.state.notes.length){
-                    this.addNote('d');
-                    this.setState({selected: this.state.notes.length});
-                }
-                break;
-            
-        }
-        
-    }
-
-    addNote(note){
-        console.log(note);
-        let nextNotes = this.state.notes;
-        nextNotes.push(note);
-        console.log(nextNotes);
-        this.setState({notes: nextNotes});
-
-        localStorage.setItem("notes", JSON.stringify(nextNotes));
-        console.log(this.state.notes);
-    }
-
-    updateNote(index, newNote){
-        let nextNotes = this.state.notes;
-        nextNotes[index] = newNote;
-        this.setState({notes: nextNotes});
-
-        localStorage.setItem("notes", JSON.stringify(nextNotes));
-        console.log(this.state.notes);
-
-    }
-
-    removeNote(index){
-        let nextNotes = this.state.notes;
-        nextNotes.splice(index, 1);
-
-        this.setState({notes: nextNotes});
-
-        localStorage.setItem("notes", JSON.stringify(nextNotes));
-    }
-
-    render(){
-        return(
-            <React.Fragment>
-                    <a href="https://www.freepik.com/free-vector/vintage-ornaments-kit_1155572.htm">
-                        <img src={Ornament} alt="Ornament Designed by Asmaarzq" className="ornament"/>
-                    </a>
-                    <Header>
-                        <h1>{this.state.title}</h1>
-                        <p>{this.state.description}</p>
-                    </Header>
-                    <div className="noteGroup">
-                        {this.state.notes.map((note, i) => {
-                            return <Note note={note} selected={i==this.state.selected} onClick={() => this.removeNote(i)} key={i}/>
-                        })}
-
-                        <AddNote selected={this.state.selected == this.state.notes.length} action={() => this.addNote('d')}/>
-                    </div>
-
-                
-            </React.Fragment>
+      //Consider if these should loop inifintely
+      case "ArrowUp":
+        this.updateNote(
+          this.state.selected,
+          orderOfNotes[
+            currentIndex == orderOfNotes.length - 1
+              ? orderOfNotes.length - 1
+              : currentIndex + 1
+          ]
         );
+        break;
+      case "ArrowDown":
+        this.updateNote(
+          this.state.selected,
+          orderOfNotes[currentIndex == 0 ? 0 : currentIndex - 1]
+        );
+        break;
+      case "Backspace":
+      case "Delete":
+        this.removeNote(this.state.selected);
+        if (this.state.selected == this.state.notes.length) {
+          this.setState({ selected: this.state.notes.length - 1 });
+        }
+        break;
+      case "Enter":
+        if (this.state.selected == this.state.notes.length) {
+          this.addNote("d");
+          this.setState({ selected: this.state.notes.length });
+        }
+        break;
     }
+  }
+
+  addNote(note) {
+    console.log(note);
+    let nextNotes = this.state.notes;
+    nextNotes.push(note);
+    console.log(nextNotes);
+    this.setState({ notes: nextNotes });
+
+    localStorage.setItem("notes", JSON.stringify(nextNotes));
+    console.log(this.state.notes);
+  }
+
+  updateNote(index, newNote) {
+    let nextNotes = this.state.notes;
+    nextNotes[index] = newNote;
+    this.setState({ notes: nextNotes });
+
+    localStorage.setItem("notes", JSON.stringify(nextNotes));
+    console.log(this.state.notes);
+  }
+
+  removeNote(index) {
+    let nextNotes = this.state.notes;
+    nextNotes.splice(index, 1);
+
+    this.setState({ notes: nextNotes });
+
+    localStorage.setItem("notes", JSON.stringify(nextNotes));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header>
+          <h1>{this.state.title}</h1>
+          <p>{this.state.description}</p>
+        </Header>
+        <div className="noteGroup">
+          {this.state.notes.map((note, i) => {
+            return (
+              <Note
+                note={note}
+                selected={i == this.state.selected}
+                onClick={() => this.removeNote(i)}
+                key={i}
+              />
+            );
+          })}
+
+          <AddNote
+            selected={this.state.selected == this.state.notes.length}
+            action={() => this.addNote("d")}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 Song.propTypes = {
-    notes: PropTypes.arrayOf(PropTypes.string),
-    title: PropTypes.string
+  notes: PropTypes.arrayOf(PropTypes.string),
+  title: PropTypes.string
 };
